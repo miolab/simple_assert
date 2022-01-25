8d15bdbb5c2c275e2ce2c24a5e91ba24885f92c7
im <poson_hemp@yahoo.co.jp>
2022-01-25 09:36:55 +0900

    feature/ def assert(e)

--- lib/simple_assert.ex
+++ lib/simple_assert.ex
@@ -2,4 +2,26 @@ defmodule SimpleAssert do
   @moduledoc """
   Documentation for `SimpleAssert`.
   """
+
+  @doc """
+  Asserts the `e` is `true`.
+
+  If it succeeds, it returns `:ok`. Raises an error otherwise.
+
+  ## Examples
+
+      iex> SimpleAssert.assert(1 == 1)
+      :ok
+
+      iex> SimpleAssert.assert(1 == 2)
+      ** (ArgumentError) assertion failed!
+  """
+  @spec assert(boolean()) :: :ok | none()
+  def assert(e) do
+    if e do
+      :ok
+    else
+      raise ArgumentError, "assertion failed!"
+    end
+  end
 end
